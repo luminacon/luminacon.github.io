86370e0570ef762ede1faa48c9e910f343b11a3f
Kishore Kumar Vivekanandan <globalalumni.initiative@gmail.com>
2021-12-14 15:39:26 +0100

    initial commit
    initial commit

---
+++ controller/Home.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"../model/formatter",
+	"sap/m/MessageToast"
+], function(Controller, formatter, m) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
+
+		formatter: formatter,
+
+		onInit: function () {
+
+		},
+
+		onPress: function () {
+			
+		}
+
+	});
+});
